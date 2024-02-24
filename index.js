e36eeeb358f1cbe7c609d7eed4a3c69d3040df38
noppakornn <noppakorn.5142@gmail.com>
2024-02-24 21:55:52 +0700

    update

--- index.js
+++ index.js
@@ -88,4 +88,6 @@ app.get('/delete/:id',async(req,res)=>{
   }
 });
 
-app.listen(5500,()=> console.log(`Listening on port 5500`))
+app.listen(5500, () => {
+    console.log(`Listening on port 5500`)
+});
