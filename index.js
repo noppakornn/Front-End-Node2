bad8943f5169022e0a55a791e29706f199afb397
noppakornn <noppakorn.5142@gmail.com>
2024-02-24 21:53:50 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 const express = require('express')
 const axios = require('axios')
-const path = require('path')
+//const path = require('path')
 const app = express()
 var bodyParser = require('body-parser')
 
@@ -13,7 +13,7 @@ const base_url = "http://localhost:3000"
 //const base_url = "http://node58141-noderestjame.proen.app.ruk-com.cloud"
 
 // Set the template engine
-app.set('views', path.join(__dirname, "/public/views"))
+//app.set('views', path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:false}))
