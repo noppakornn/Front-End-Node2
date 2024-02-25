32acae00eda132d00b4eac6277532e1147a41c0a
noppakornn <noppakorn.5142@gmail.com>
2024-02-25 20:51:47 +0700

    update

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser')
 
 //const base_url = "http://localhost:3000"
 //const base_url = "http://10.104.7.149"
-const base_url = "http://node58141-noderestjame.proen.app.ruk-com.cloud"
+const base_url = "http://node58141-noderestjame.proen.app.ruk-com.cloud:11291"
 
 // Set the template engine
 app.set('views', path.join(__dirname, "/public/views"))
