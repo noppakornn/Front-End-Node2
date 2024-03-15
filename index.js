769f346e15811267b7e66cfdda23c1c3308fc765
noppakornn <noppakorn.5142@gmail.com>
2024-03-15 11:58:20 +0700

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser')
 
 //const base_url = "http://localhost:3000"
 //const base_url = "http://10.104.7.149"
-const base_url = "http://node58143-noderestjame.proen.app.ruk-com.cloud:11713/Restaurant";
+const base_url = "http://node58143-noderestjame.proen.app.ruk-com.cloud/Restaurants";
 
 app.set('views', path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs')
